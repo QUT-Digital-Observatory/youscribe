82d859f20e4274337835668403fb93620e5a0ed3
Boyd Nguyen <thaihoang.nguyen@qut.edu.au>
2024-03-20 16:44:16 +1000

    refactor WIP

--- src/transcribe.py
+++ src/transcribe.py
@@ -2,20 +2,33 @@
 Copyright: Digital Observatory 2023 <digitalobservatory@qut.edu.au>
 Author: Mat Bettinson <mat.bettinson@qut.edu.au>
 """
+
 import logging
 from datetime import timedelta
 from os import path, remove
 from time import time
 
-from youteplustrans.scraper import get_VideoDetails
-from youteplustrans.whisper import ShortSegment, WhisperTranscribe
 from yt_dlp import YoutubeDL
+
+from .scraper import get_video_details
+from .whisper import ShortSegment, WhisperTranscribe
 
 logger = logging.getLogger(__name__)
 
 
-class YouteTranscriber:
+class Youscribe:
     def __init__(self, url: str, model: str = "small", cpu_threads: int = 4):
+        """Initiliase Youscribe class and specify the model for Whisper transcription
+
+        Parameters
+        ----------
+        url : str
+            _description_
+        model : str, optional
+            _description_, by default "small"
+        cpu_threads : int, optional
+            _description_, by default 4
+        """
         self.url = url
         self.model = model
         self.threads = cpu_threads
@@ -25,32 +38,32 @@ class YouteTranscriber:
 
     def transcribe(self) -> list[ShortSegment] | None:
         """Execute the transcription job, saving output as appropriate"""
-        starttime = time()
-        videodetails = get_VideoDetails(self.url)
-        if videodetails is None:
+        start_time = time()
+        video_details = get_video_details(self.url)
+        if video_details is None:
             logger.error("Failed to get video details")
             return None
-        logger.info("Got metadata in" + self._get_elapsed(starttime, time()))
+        logger.info("Got metadata in" + self._get_elapsed(start_time, time()))
 
         prompt = (
-            videodetails.title + ": " + videodetails.shortDescription
+            video_details.title + ": " + video_details.shortDescription
         )  # Will feed to Whisper API
         ydl_opts = {"format": "worstaudio[ext=webm]", "outtmpl": "%(id)s.%(ext)s"}
-        sourcefilename = videodetails.videoId + ".webm"
+        source_file = video_details.videoId + ".webm"
 
-        if not path.isfile(sourcefilename):
-            starttime = time()
+        if not path.isfile(source_file):
+            start_time = time()
             with YoutubeDL(ydl_opts) as ydl:
                 ydl.download([self.url])
-            logger.info("Downloaded in" + self._get_elapsed(starttime, time()))
+            logger.info("Downloaded in" + self._get_elapsed(start_time, time()))
         # <id>.webm in the current directory now
 
-        starttime = time()
+        start_time = time()
         logger.info("Transcribing...")
         transcriber = WhisperTranscribe(
-            sourcefilename, prompt, self.model, cpu_threads=self.threads
+            source_file, prompt, self.model, cpu_threads=self.threads
         )
-        logger.info("Whisper transcribed in" + self._get_elapsed(starttime, time()))
+        logger.info("Whisper transcribed in" + self._get_elapsed(start_time, time()))
 
-        remove(sourcefilename)
+        remove(source_file)
         return transcriber.segments
---
+++ src/transcript.py
@@ -0,0 +1,120 @@
+import json
+from abc import ABC, abstractmethod
+from datetime import timedelta
+from pathlib import Path
+from typing import Iterable
+
+from faster_whisper.transcribe import Segment
+
+
+class BaseTranscript(ABC):
+    @abstractmethod
+    def text(self):
+        pass
+
+    @abstractmethod
+    def csv(self):
+        pass
+
+    @abstractmethod
+    def json(self):
+        pass
+
+    @abstractmethod
+    def srt(self):
+        pass
+
+
+class WhisperTranscript(BaseTranscript):
+    def __init__(self, segments: Iterable[Segment]):
+        self.segments = list(segments)
+
+    def text(self) -> str:
+        return " ".join([segment.text.strip() for segment in self.segments])
+
+    def json(self) -> str:
+        obj = []
+        for segment in self.segments:
+            obj.append(
+                {
+                    "id": segment.id,
+                    "start": round(segment.start, 2),
+                    "end": round(segment.end, 2),
+                    "text": segment.text.strip(),
+                }
+            )
+        return json.dumps(obj)
+
+    def srt(self, file_path: str | Path | None = None) -> str:
+        srt_str = ""
+
+        for i, segment in enumerate(self.segments):
+            # Convert start and end times to hh:mm:ss,sss format
+            start_time = timedelta(seconds=segment.start)
+            end_time = timedelta(seconds=segment.end)
+
+            # Convert timedelta to appropriate string format
+            str_start_time = str(start_time).zfill(8)
+            if "." in str_start_time:
+                str_start_time = str_start_time.replace(".", ",")
+                if len(str_start_time.split(",")[1]) < 3:
+                    str_start_time = str_start_time + "0" * (
+                        3 - len(str_start_time.split(",")[1])
+                    )
+            else:
+                str_start_time = str_start_time + ",000"
+
+            str_end_time = str(end_time).zfill(8)
+            if "." in str_end_time:
+                str_end_time = str_end_time.replace(".", ",")
+                if len(str_end_time.split(",")[1]) < 3:
+                    str_end_time = str_end_time + "0" * (
+                        3 - len(str_end_time.split(",")[1])
+                    )
+            else:
+                str_end_time = str_end_time + ",000"
+
+            # Add index, time range, and text to the string
+            srt_str += f"{i+1}\n{str_start_time} --> {str_end_time}\n{segment.text.strip()}\n\n"
+
+        if file_path:
+            with open(file_path, "w") as f:
+                f.write(srt_str)
+
+        return srt_str
+
+    # def segments(self) -> list[ShortSegment]:
+    #     short_segments = []
+    #     for segment in self.segments:
+    #         short_segments.append(
+    #             ShortSegment(
+    #                 id=segment.id,
+    #                 start=round(segment.start, 2),
+    #                 end=round(segment.end, 2),
+    #                 text=segment.text.strip(),
+    #             )
+    #         )
+    #     return short_segments
+
+    # def csv(self) -> str | None:
+    #     try:
+    #         output = io.StringIO()
+    #         writer = csv.DictWriter(
+    #             output, fieldnames=["id", "start", "end", "text"], quoting=csv.QUOTE_ALL
+    #         )
+    #         writer.writeheader()
+    #         for segment in self.segmentlist:
+    #             seg = {
+    #                 "id": segment.id,
+    #                 "start": round(segment.start, 2),
+    #                 "end": round(segment.end, 2),
+    #                 "text": segment.text.strip(),
+    #             }
+    #             # encode special characters in the 'text' field
+    #             seg["text"] = seg["text"].encode("unicode_escape").decode("utf-8")
+    #             writer.writerow(seg)
+    #         return output.getvalue()
+    #     except Exception as e:
+    #         logger.error("An error occurred while creating the CSV string.")
+    #         logger.error(str(e))
+    #         return None
